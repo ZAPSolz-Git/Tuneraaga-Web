9e8b2865c990af90e46d968d854dde5826609c72
AAkash001122 <261405989+zapsolution-hub@users.noreply.github.com>
2026-07-24 16:38:26 +0530

    added more cors address

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "http://localhost:5174",
+      "http://localhost:8081",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
